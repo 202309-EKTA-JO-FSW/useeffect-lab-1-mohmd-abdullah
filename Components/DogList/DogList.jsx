108bd4a61dc2613e2ec5a6527f4380785bdc1534
Mohammad <mo7amed.2197@gmail.com>
2023-11-02 06:04:19 +0300

    Done

--- Components/DogList/DogList.jsx
+++ Components/DogList/DogList.jsx
@@ -1,6 +1,7 @@
 
 
 export default function ShoppingList({ dogsList = [] }) {
+  //console.log('dogsList' ,dogsList)
   return (
     <div className="shopping-cart">
       <h3 style={{ textAlign: "center" }}>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import DogList from '../Components/DogList/DogList'
 import Form from '../Components/Form/Form'
 import { Inter } from 'next/font/google'
@@ -7,17 +8,32 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   // You will need to put a state here to save all the dogs data into
+  const [data, setData] = useState(!null);
+  const [inputText, setInputText] = useState("")
   // And you will fetch the data with useEffect
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random/${inputText}')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+      })
+  }, [])
+  //console.log("ID: ", data.id);
+  //console.log(data.id);
 
-   return (
+  function formInput(input) {
+    setInputText(input)
+  }
+  
+  return (
     <div className="card">
       {/* When the button is clicked in the form, it should fetch the information. 
-          How can we do that by utilizing useState?
-          
-      */}
+          How can we do that by utilizing useState?*/
+      }
+      <Form setNumberOfDogs={formInput} />
       {/* <Form /> Uncomment <Form /> if you are going after the bonus */}
       {/* This page should receive the images array */}
-      <DogList />
+      <DogList dogsList={data['message']} />
     </div>
   );
 }
